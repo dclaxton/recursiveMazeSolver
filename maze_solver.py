f7fe9738fe8c64615b8125a2e37cbfaa3c28570e
dalton <dclaxton1@my.apsu.edu>
2020-04-04 22:53:22 -0500

    reorganized code, fixed minor bugs involving entry,exit finding

--- maze_solver.py
+++ maze_solver.py
@@ -4,92 +4,99 @@ import sys
 #Maze generator used for mazes:
 # http://hereandabove.com/maze/mazeorig.form.html
 
-#open our png and get it ready to draw on, also set our recursive limit higher than the default (1000)
-crop = ImageOps.crop(Image.open('maze.png').convert("RGB"), (1,1,1,1))  
-draw = ImageDraw.Draw(crop)
-sys.setrecursionlimit(10000)
-
-
 #recursive method
-def solveRecursiveMaze(arr,x,y):
+def solveRecursiveMaze(arr, width, height, x,y):
     successful = False
-    if (x,y) == (exitX,exitY):
+    if (x,y) == getExitPoint(arr,width,height):
         successful = True
-    elif isValid(arr,x,y):
+    elif isValid(arr,width,height,x,y):
         arr[x][y] = "V" #set to V to show it's a visited path
-        successful = solveRecursiveMaze(arr, x-1, y)    
+        successful = solveRecursiveMaze(arr, width, height, x-1, y)  
         if not successful:
-            successful = solveRecursiveMaze(arr, x, y+1)
+            successful = solveRecursiveMaze(arr, width, height,x, y+1)
         if not successful:
-            successful = solveRecursiveMaze(arr, x+1, y)
+            successful = solveRecursiveMaze(arr, width, height,x+1, y)
         if not successful:
-            successful = solveRecursiveMaze(arr, x, y-1)
+            successful = solveRecursiveMaze(arr, width, height,x, y-1)  
     if successful:
-        arr[x][y] = "P" #Mark as P to show it's a valid pa
+        arr[x][y] = "P" #Mark as P to show it's a valid path
     return successful
 
 #are the values in the array, and is it a valid path
-def isValid(arr,x,y):
-    if x < len(arr) and y < len(arr) and x >= 0 and y >= 0:
+def isValid(arr,width,height,x,y):
+    if x < width and y < height and x >= 0 and y >= 0:
         if arr[x][y] == "W":
             return True
     return False
 
 #find the entryPoint of the maze     
-def getEntryPoint():
-    x = 0
-    for y in range(0, crop.size[1] - 1):
-        if(crop.getpixel((x,y)) == (255,255,255)):
-            return x,y
-        if(crop.getpixel((y,x)) == (255,255,255)):
-            return y,x
+def getEntryPoint(arr, width, height):
+    #traverse along top
+    for x in range(1, width - 1):
+        if(arr[x][1] == 'W'):
+            return x,1
+
+    #traverse left side
+    for y in range(1, height - 1):
+        if(arr[1][y] == 'W'):
+            return 1,y
 
 #find the exitPoint of the maze
-def getExitPoint():
-    x = crop.size[0] - 1
-    for y in range(0, crop.size[1] - 1):
-        if(crop.getpixel((x,y)) == (255,255,255)):
-            return x,y
+def getExitPoint(arr, width, height):
+    #Traverse bottom
+    for x in range(1, width - 1):
+        if arr[x][height - 2] == 'W':
+            return x, height - 2
+
+    #Traverse right side
+    for y in range(1, height - 1):
+        if arr[width - 2][y] == 'W':
+            return width - 2,y
 
 #Loop through the array and draw the solution
-def drawSolution(arr):
-    for i in range(1, crop.size[0] - 1):
-        for j in range(1, crop.size[1] - 1):
-            if arr[i][j] == "P":
-                if arr[i+1][j] == "P":
-                    draw.line([(i,j),(i+1,j)],fill=128,width=1)
-                elif arr[i-1][j] == "P":
-                    draw.line([(i,j),(i-1,j)],fill=128,width=1)
-                elif arr[i][j+1] == "P":
-                    draw.line([(i,j),(i,j+1)],fill=128,width=1)
-                elif arr[i][j-1] == "P":
-                    draw.line([(i,j),(i,j-1)],fill=128,width=1)
+def drawSolution(arr,width,height):
+    newImg = Image.new('RGB',(width,height),(0,0,0))
+    for x in range(width):
+        for y in range(height):
+            charToRGB = {
+                'W' : (255,255,255),
+                'B' : (0,0,0),
+                'P' : (255,0,0),
+                'V' : (255,255,255)
+            }
+            newImg.putpixel((x, y),charToRGB[arr[x][y]])
+    newImg.save("solution.png","png")
 
-#2D array of each pixel, "W" representing a white pixel. "B" is representing a black pixel        
-maze = []
-width,height = crop.size
-for x in range(0, width):
-    mazeX = []
-    for y in range(0, height):
-        if(crop.getpixel((x,y)) == (0,0,0)):
-            mazeX.append("B")
-        elif(crop.getpixel((x,y)) == (255,255,255)):
-            mazeX.append("W")
-    maze.append(mazeX)
 
-#get X,Y pixels of both entry and exit points
-entryX,entryY = getEntryPoint()
-exitX,exitY = getExitPoint()
-#if it is solved, draw the solution, if not just draw the entry/exit points
-if solveRecursiveMaze(maze,entryX,entryY):
-    print("Solved")
-    draw.line([(exitX,exitY),(exitX-1,exitY)],fill=128,width=1)
-    draw.line([(entryX,entryY),(entryX-1,entryY)],fill=128,width=1)
-    drawSolution(maze)
-else:
-    print("Not Solved")
-    draw.line([(exitX,exitY),(exitX-1,exitY)],fill=128,width=1)
-    draw.line([(entryX,entryY),(entryX-1,entryY)],fill=128,width=1)
+#2D array of each pixel, "W" representing a white pixel. "B" is representing a black pixel
+def convertImageToArray(imageName):
+    crop = Image.open(imageName).convert("RGB")
+    maze = []
+    width,height = crop.size
+    for x in range(0, width):
+        mazeX = []
+        for y in range(0, height):
+            if(crop.getpixel((x,y)) == (0,0,0)):
+                mazeX.append("B")
+            elif(crop.getpixel((x,y)) == (255,255,255)):
+                mazeX.append("W")
+        maze.append(mazeX)
+    return maze, width, height
 
-#show the completed picture
-crop.show()
+def main():
+    solved = False
+    maze,width,height = convertImageToArray("maze1.png")
+    x,y = getEntryPoint(maze,width,height)
+    print("Entering maze at x = %d, y = %d", (x,y))   
+    #if it is solved, draw the solution, if not just draw the entry/exit points
+    solved = solveRecursiveMaze(maze,width,height,x,y)
+    if not solved:
+        print("Cannot find solution")
+    else:
+        print("Solution found!")
+        drawSolution(maze,width,height)
+    return 0 if solved else 1
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(15000)
+    sys.exit(main())
